8b3f8de1afba0d34216e46bc872a383f1b95c12b
MJ Tier <michael.j.tier@gmail.com>
2021-05-30 20:23:17 -0400

    Initial project commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Michael Tier on 5/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
